d17fe678abc1f4941abb6a50c6a4e91c0cdbf380
Jordan Heffernan <jheff2@gmail.com>
2018-12-03 13:18:17 -0800

    added beyond function

--- app.js
+++ app.js
@@ -3,3 +3,15 @@ function jediName(firstName,lastName){
     let fiTwo = firstName.slice(0,2)
     return fiThree + fiTwo;
 }
+
+function beyond (num) {
+    if (num === Infinity || num === -Infinity) {
+        console.log('And beyond');
+    } else if (num > 0){
+        console.log('To infinity');
+    } else if (num < 0) {
+        console.log('To negative infinity');
+    } else if (num === 0) {
+        console.log('Staying home');
+    }
+}
