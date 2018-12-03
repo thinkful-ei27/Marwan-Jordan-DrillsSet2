df94489ca2cee3f57bcd86a3b0cf43e945ac1ad1
Marwan Alaloosi <manoalosi@gmail.com>
2018-12-03 15:39:11 -0600

    uncoded char

--- app.js
+++ app.js
@@ -15,3 +15,22 @@ function beyond (num) {
         console.log('Staying home');
     }
 }
+
+function decode(uncoded) {
+    let newArr = []
+    for (let i = 0; i < uncoded.length; i++){
+        if(uncoded[0] === "a"){
+            newArr.push(uncoded[1])
+        } else if (uncoded[0] === "b"){
+            newArr.push(uncoded[2])
+        } else if(uncoded[0] === "c"){
+            newArr.push(uncoded[3])
+        } else if (uncoded[0] === 'd'){
+            newArr.push(uncoded[4])
+        } else {
+            return " "
+        }
+    }
+    return newArr[i];
+}
+
